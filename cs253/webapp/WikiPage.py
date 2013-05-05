a00394957936ebeeb880cd5e8fd9ad569a5c8adb
Simon Buist <simon.buist@gmail.com>
2013-05-05 21:27:10 +0100

    Remove ugly hack
    This page.render() call used to store in a render_str_ variable the
    result of substituting all newlines in the page.content with <br> tags.
    Ugly.
    Now we use Markdown!

--- cs253/webapp/WikiPage.py
+++ cs253/webapp/WikiPage.py
@@ -3,7 +3,6 @@ from WikiHandler import WikiHandler
 class WikiPage(WikiHandler):
     def done(self, page = "", path = ""):
         if page:
-            page.render()
             self.render("page.html", page = page, path = path)
         else: 
             self.redirect('/wiki/_edit' + path)
