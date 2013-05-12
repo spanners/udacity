0b0a9aa3d0fef22b118750b39c712bd2c5b161f4
Simon Buist <hashcollide@gmail.com>
2013-05-12 18:21:31 +0100

    Move the BlogFront Handler into the handlers/ package

---
+++ cs253/webapp/handlers/BlogFront.py
@@ -0,0 +1,10 @@
+from handlers.user import UserHandler
+from lib.db.Post import get_posts, age_str
+
+class BlogFront(UserHandler):
+    def get(self):
+        posts, age = get_posts()
+        if self.format == 'html':
+            self.render('blogfront.html', posts = posts, age = age_str(age))
+        elif self.format == 'json':
+            return self.render_json([p.as_dict() for p in posts])
